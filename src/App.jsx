f9772f20138c373822c3a7275b2e19e515e3a619
Nishad Kindre <nkindre@medtigo.com>
2025-06-05 10:48:41 +0530

    refactor: new theme folder for mui theme object

--- src/App.jsx
+++ src/App.jsx
@@ -15,56 +15,7 @@ import { LicenseContextProvider } from "./context/LicenseContext";
 import { ReportFilterContextProvider } from "./context/ReportFilterContext";
 import history from "./utils/history";
 import { CONNECT_URL } from "./settings";
-
-// Default light theme
-const theme = createTheme({
-  palette: {
-    mode: "light", // Enforce light theme
-  },
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        body: {
-          // Enforce light theme scrollbars
-          scrollbarColor: '#888 #fff',
-          '&::-webkit-scrollbar-thumb': {
-            backgroundColor: '#888',
-          },
-          '&::-webkit-scrollbar-thumb:hover': {
-            backgroundColor: '#bbb',
-          },
-        },
-      },
-    },
-    MuiButtonBase: {
-      styleOverrides: {
-        root: {
-          '&:focus, &:focus-visible': {
-            outline: "none",
-          },
-        },
-      },
-    },
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          "&:focus-within": {
-            outline: "none",
-          },
-        },
-      },
-    },
-    MuiTab: {
-      styleOverrides: {
-        root: {
-          "&:focus": {
-            outline: "none",
-          },
-        },
-      },
-    },
-  },
-});
+import { theme } from "./theme";
 
 function App() {
   const params = new URLSearchParams(window.location.search);
